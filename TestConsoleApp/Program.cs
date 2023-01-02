6ce2a8872a1175755c3e179c39de73130bc84b63
Bipul <Bipulkumarbk@gmail.com>
2023-01-02 14:00:14 +0530

    Pushing the all file first time.

---
+++ TestConsoleApp/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestConsoleApp
+{
+    abstract class TestAbst
+    {
+        public void TestMed()
+        {
+            Console.WriteLine("Hello World");
+            Console.ReadLine();
+        }
+        public abstract void TestAbstMed();
+       
+    }
+
+    class ImplementTest:TestAbst
+    {
+        public override void TestAbstMed()
+        {
+            Console.WriteLine("Hello World I am Abstract");
+            Console.ReadLine();
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ImplementTest test = new ImplementTest();
+
+            test.TestMed();
+            test.TestAbstMed();
+
+                 
+        }
+    }
+}
